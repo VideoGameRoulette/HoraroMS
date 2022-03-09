beab7ed415eaaa191bb405703ce3e08ed430832e
unknown <videogameroulette@yahoo.ca>
2022-03-09 18:09:44 -0500

    caddy support added

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/schedule', async (req, res) => {
+app.get('/api/schedule', async (req, res) => {
     const { data: schedule } = await axios.get(`https://horaro.org/-/api/v1/schedules/${process.env.HORARO_ID}`);
     return res.json(schedule.data);
 });
 
-app.get('/schedule/:id', async (req, res) => {
+app.get('/api/schedule/:id', async (req, res) => {
     const { id } = req.params;
     const { data: schedule } = await axios.get(`https://horaro.org/-/api/v1/schedules/${id}`);
     return res.json(schedule.data);
